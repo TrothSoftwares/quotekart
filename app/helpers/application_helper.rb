d16a47ba6ecf23f2f0147976a575f20d08b135cf
Mohammed Talha <talhaqqq@gmail.com>
2015-08-26 21:05:50 +0530

    devise installed roles views

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,20 @@
+module ApplicationHelper
+
+ ## for devise
+  def resource_name
+   :user
+end
+
+def resource
+   @resource ||= User.new
+end
+
+def devise_mapping
+   @devise_mapping ||= Devise.mappings[:user]
+end
+  
+  
+end
+
+
+  
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :dealers
+  devise_for :admins
+  devise_for :users
+   
+  
+ 
+   
+   
+end
