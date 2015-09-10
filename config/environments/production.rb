72950fb824d816c7d097e7fc6815f01fbd898e67
Mohammed Talha <talhaqqq@gmail.com>
2015-09-10 10:58:49 +0530

    gmail smtp configured for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+    config.action_mailer.default_url_options = { host: 'quotekart.herokuapp.com', port: 3000 }
+  
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => 'talha@object90.com',
+  :password       => '9947183255p',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+  
 end
